d3d3d6574addd1de4e71824248c29939e5b56b54
injifann <gutemaurgawa@gmail.com>
2026-07-16 15:14:04 +0300

    moved images inside public folder

--- portifolio/src/data/projects.js
+++ portifolio/src/data/projects.js
@@ -1,39 +1,20 @@
-import pro1image1 from '../assets/images/dashboard2.png'
-import pro1image2 from '../assets/images/addform.png'
-import pro1image3 from '../assets/images/updateform.png'
-import pro2image1 from '../assets/images/Dashboard.png'
-import pro2image2 from '../assets/images/Login.png'
-import pro2image3 from '../assets/images/Register.png'
-import pro3image3 from '../assets/images/Screenshot (68).png'
-import pro3image1 from '../assets/images/Screenshot (69).png'
-import pro3image2 from '../assets/images/Screenshot (70).png'
-import pro4image4 from '../assets/images/Screenshot (71).png'
+// import pro1image1 from '/kabe_gutema_new/images/dashboard2'
+// import pro1image2 from '../assets//kabe_gutema_new/images/addform.png'
+// import pro1image3 from '../assets/images/updateform.png'
+// import pro2image1 from '../assets/images/Dashboard.png'
+// import pro2image2 from '../assets/images/Login.png'
+// import pro2image3 from '../assets/images/Register.png'
+
 
 
 export const projects = [
-    {   id:1,
-        title: "ReadTrack MERN App",
-        description:"A full-stack MERN application that allows users to track books they have read. Users can create, view, update, and delete their reading list.",
-        images:[pro1image1,pro1image2,pro1image3],
-        techStack:["React","Express","MongoDb","Node.js","Tailwind css"],
-        liveurl:"",
-        githublink:"https://github.com/injifann/readtrack-mernapp"
-    },
 
-        {id:2,
-        title: "MERN Authentication",
-        description:"A secure authentication system built with the MERN stack. Features include user registration, login, JWT authentication, and Google OAuth sign-in.",
-        images:[pro2image1,pro2image2,pro2image3],
-        techStack:["React","Express","MongoDb","Node.js","Tailwind css"],
-        liverl:"",
-        githublink:"https://github.com/injifann/authentication_mern"
-    },
-     {
+  {
     id: 3,
     title: "Poultry E-Commerce",
     description:
       "A full-stack MERN e-commerce application for poultry products featuring separate user and admin interfaces. Includes product and category management, shopping cart, order processing, address management, image uploads with Cloudinary, inventory validation, and secure JWT & Google OAuth authentication.",
-    images: [pro3image1, pro3image2, pro3image3, pro4image4],
+    images: ['/kabe_gutema_new/images/productdetailspage.png','/kabe_gutema_new/images/shoppage.png','/kabe_gutema_new/images/profilepage.png','/kabe_gutema_new/images/adminproductpage.png','/kabe_gutema_new/images/adminusermanagement.png','/kabe_gutema_new/images/cartpage.png'],
     techStack: [
       "React",
       "Node.js",
@@ -42,9 +23,27 @@ export const projects = [
       "Tailwind CSS",
       "Cloudinary"
     ],
-    liverl: "",
+    liveurl: "https://mern-poulty-ecommerce-rosy.vercel.app",
     githublink: "https://github.com/injifann/mern_poulty_ecommerce"
-}
+},
+    
+  {   id:1,
+        title: "ReadTrack MERN App",
+        description:"A full-stack MERN application that allows users to track books they have read. Users can create, view, update, and delete their reading list.",
+        images:['/kabe_gutema_new/images/dashboard2.png','/kabe_gutema_new/images/addform.png','/kabe_gutema_new/images/Login.png'],
+        techStack:["React","Express","MongoDb","Node.js","Tailwind css"],
+        liveurl:"",
+        githublink:"https://github.com/injifann/readtrack-mernapp"
+    },
+
+        {id:2,
+        title: "MERN Authentication",
+        description:"A secure authentication system built with the MERN stack. Features include user registration, login, JWT authentication, and Google OAuth sign-in.",
+        images:['/kabe_gutema_new/images/Dashboard.png','/kabe_gutema_new/images/Register.png','/kabe_gutema_new/images/updateform.png'],
+        techStack:["React","Express","MongoDb","Node.js","Tailwind css"],
+        liveurl:"",
+        githublink:"https://github.com/injifann/authentication_mern"
+    },
     
 ]
 
